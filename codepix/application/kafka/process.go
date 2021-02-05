fbfe292b17518fdc75857d36e5f2ce4db88800d6
Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
2021-02-05 13:35:53 -0300

    add process transaction confirmation

--- codepix/application/kafka/process.go
+++ codepix/application/kafka/process.go
@@ -6,6 +6,7 @@ import (
 	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
 	appmodel "github.com/erickrodrigs/codepix/codepix-go/application/appmodel"
 	"github.com/erickrodrigs/codepix/codepix-go/application/factory"
+	"github.com/erickrodrigs/codepix/codepix-go/application/usecase"
 	"github.com/erickrodrigs/codepix/codepix-go/domain/model"
 	"github.com/jinzhu/gorm"
 )
@@ -63,6 +64,7 @@ func (kp *MyKafkaProcessor) processMessage(msg *ckafka.Message) {
 	case transactionsTopic:
 		kp.processTransaction(msg)
 	case transactionConfirmationTopic:
+		kp.proccessTransactionConfirmation(msg)
 	default:
 		fmt.Println("not a valid topic", string(msg.Value))
 	}
@@ -94,6 +96,56 @@ func (kp *MyKafkaProcessor) processTransaction(msg *ckafka.Message) error {
 	topic := "bank" + createdTransaction.PixKeyTo.Account.Bank.Code
 	transaction.ID = createdTransaction.ID
 	transaction.Status = model.TransactionPending
+	transactionJSON, err := transaction.ToJSON()
+
+	if err != nil {
+		return err
+	}
+
+	err = Publish(string(transactionJSON), topic, kp.Producer, kp.DeliveryChan)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (kp *MyKafkaProcessor) proccessTransactionConfirmation(msg *ckafka.Message) error {
+	transaction := appmodel.NewTransaction()
+	err := transaction.ParseJSON(msg.Value)
+
+	if err != nil {
+		return err
+	}
+
+	transactionUseCase := factory.TransactionUseCaseFactory(kp.Database)
+
+	if transaction.Status == model.TransactionConfirmed {
+		err = kp.confirmTransaction(transaction, transactionUseCase)
+
+		if err != nil {
+			return err
+		}
+	} else if transaction.Status == model.TransactionCompleted {
+		_, err := transactionUseCase.Complete(transaction.ID)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (kp *MyKafkaProcessor) confirmTransaction(transaction *appmodel.Transaction, transactionUseCase usecase.TransactionUseCase) error {
+	confirmedTransaction, err := transactionUseCase.Confirm(transaction.ID)
+
+	if err != nil {
+		return err
+	}
+
+	topic := "bank" + confirmedTransaction.AccountFrom.Bank.Code
 	transactionJSON, err := transaction.ToJSON()
 
 	if err != nil {
