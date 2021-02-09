c8dd0356b048b253608851673c286ef6d001f76a
Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
2021-02-09 18:16:29 -0300

    add transaction confirmation for bank api
    the transaction's process will communicate with codepix via apache kafka, changing the status of the transaction until it is completed

--- bank-api/src/controllers/transaction/transaction.controller.ts
+++ bank-api/src/controllers/transaction/transaction.controller.ts
@@ -10,26 +10,29 @@ import {
   Post,
   ValidationPipe,
 } from '@nestjs/common';
-import { ClientKafka } from '@nestjs/microservices';
+import { ClientKafka, MessagePattern, Payload } from '@nestjs/microservices';
 import { Producer } from '@nestjs/microservices/external/kafka.interface';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TransactionDto } from 'src/dto/transaction.dto';
 import { BankAccount } from 'src/models/bank-account.model';
+import { PixKey } from 'src/models/pix-key.model';
 import {
   Transaction,
   TransactionOperation,
+  TransactionStatus,
 } from 'src/models/transaction.model';
 import { Repository } from 'typeorm';
 
 @Controller('bank-accounts/:bankAccountId/transactions')
 export class TransactionController implements OnModuleInit, OnModuleDestroy {
   private kafkaProducer: Producer;
-
   constructor(
     @InjectRepository(BankAccount)
     private bankAccountRepo: Repository<BankAccount>,
     @InjectRepository(Transaction)
     private transactionRepo: Repository<Transaction>,
+    @InjectRepository(PixKey)
+    private pixKeyRepo: Repository<PixKey>,
     @Inject('TRANSACTION_SERVICE')
     private kafkaClient: ClientKafka,
   ) {}
@@ -78,7 +81,6 @@ export class TransactionController implements OnModuleInit, OnModuleDestroy {
       bank_account_id: bankAccountId,
       operation: TransactionOperation.debit,
     });
-
     transaction = await this.transactionRepo.save(transaction);
 
     const sendData = {
@@ -86,20 +88,91 @@ export class TransactionController implements OnModuleInit, OnModuleDestroy {
       accountId: bankAccountId,
       amount: body.amount,
       pixkeyto: body.pix_key_key,
-      pix_key_kind: body.pix_key_kind,
+      pixKeyKindTo: body.pix_key_kind,
       description: body.description,
     };
 
     await this.kafkaProducer.send({
       topic: 'transactions',
-      messages: [
-        {
-          key: 'transactions',
-          value: JSON.stringify(sendData),
-        },
-      ],
+      messages: [{ key: 'transactions', value: JSON.stringify(sendData) }],
     });
 
     return transaction;
+  }
+
+  @MessagePattern(`bank001`)
+  async doTransaction(@Payload() message) {
+    if (message.value.status === 'pending') {
+      await this.receivedTransaction(message.value);
+    }
+
+    if (message.value.status === 'confirmed') {
+      await this.confirmedTransaction(message.value);
+    }
+  }
+
+  async receivedTransaction(data) {
+    const pixKey = await this.pixKeyRepo.findOneOrFail({
+      where: {
+        key: data.pixKeyTo,
+        kind: data.pixKeyKindTo,
+      },
+    });
+
+    const transaction = this.transactionRepo.create({
+      external_id: data.id,
+      amount: data.amount,
+      description: data.description,
+      bank_account_id: pixKey.bank_account_id,
+      bank_account_from_id: data.accountId,
+      operation: TransactionOperation.credit,
+      status: TransactionStatus.completed,
+    });
+
+    this.transactionRepo.save(transaction);
+
+    const sendData = {
+      ...data,
+      status: 'confirmed',
+    };
+
+    await this.kafkaProducer.send({
+      topic: 'transaction_confirmation',
+      messages: [
+        { key: 'transaction_confirmation', value: JSON.stringify(sendData) },
+      ],
+    });
+  }
+
+  async confirmedTransaction(data) {
+    const transaction = await this.transactionRepo.findOneOrFail({
+      where: {
+        external_id: data.id,
+      },
+    });
+
+    await this.transactionRepo.update(
+      { id: data.id },
+      {
+        status: TransactionStatus.completed,
+      },
+    );
+
+    const sendData = {
+      id: data.id,
+      accountId: transaction.bank_account_id,
+      amount: Math.abs(transaction.amount),
+      pixkeyto: transaction.pix_key_key,
+      pixKeyKindTo: transaction.pix_key_kind,
+      description: transaction.description,
+      status: TransactionStatus.completed,
+    };
+
+    await this.kafkaProducer.send({
+      topic: 'transaction_confirmation',
+      messages: [
+        { key: 'transaction_confirmation', value: JSON.stringify(sendData) },
+      ],
+    });
   }
 }
